d9318d8983975aff3764ba66ba7e871a656b9bdf
Lev Vasilyev <tesrss93ee@gmail.com>
2024-04-05 10:25:47 +0300

    added variables for buttons and text

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
 
+    TextView textResult, textSolution;
+    MaterialButton buttonCos, buttonSin, buttonTan, buttonPower;
+    MaterialButton buttonC, buttonBracketOpen, buttonBracketClose, buttonDivide;
+    MaterialButton button7, button8, button9, buttonMultiply;
+    MaterialButton button4, button5, button6, buttonMinus;
+    MaterialButton button1, button2, button3, buttonPlus;
+    MaterialButton buttonAC, buttonZero, buttonDot, buttonEqual;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
