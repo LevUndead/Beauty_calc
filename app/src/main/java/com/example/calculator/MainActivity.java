f41321e6b7ee9fcb5c28d96612b98bad15787903
Lev Vasilyev <tesrss93ee@gmail.com>
2024-04-05 12:40:48 +0300

    added func and assign all buttons

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,7 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView textResult, textSolution;
     MaterialButton buttonCos, buttonSin, buttonTan, buttonPower;
@@ -25,10 +26,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        textResult = findViewById(R.id.result_text);
+        textSolution = findViewById(R.id.solution_text);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        assignId(buttonCos, R.id.button_cos);
+        assignId(buttonSin, R.id.button_sin);
+        assignId(buttonTan, R.id.button_tan);
+        assignId(buttonPower, R.id.button_power);
+        assignId(buttonC, R.id.button_c);
+        assignId(buttonBracketOpen, R.id.button_open);
+        assignId(buttonBracketClose, R.id.button_close);
+        assignId(buttonDivide, R.id.button_divide);
+        assignId(button7, R.id.button_7);
+        assignId(button8, R.id.button_8);
+        assignId(button9, R.id.button_9);
+        assignId(buttonMultiply, R.id.button_multiply);
+        assignId(button4, R.id.button_4);
+        assignId(button5, R.id.button_5);
+        assignId(button6, R.id.button_6);
+        assignId(buttonMinus, R.id.button_minus);
+        assignId(button1, R.id.button_1);
+        assignId(button2, R.id.button_2);
+        assignId(button3, R.id.button_3);
+        assignId(buttonPlus, R.id.button_plus);
+        assignId(buttonAC, R.id.button_AC);
+        assignId(buttonZero, R.id.button_zero);
+        assignId(buttonDot, R.id.button_dot);
+        assignId(buttonEqual, R.id.button_equal);
+
+    }
+    void assignId (MaterialButton button, int id){
+        button = findViewById(id);
+        button.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View View){
+
     }
 }
